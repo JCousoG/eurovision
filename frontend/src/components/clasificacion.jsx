f6f5008310b7c6229de78190b6c3e678287fbe06
Couso <jcousog@hotmail.com>
2023-06-22 19:16:00 +0200

    eliminar de clasificacion o boton

--- frontend/src/components/clasificacion.jsx
+++ frontend/src/components/clasificacion.jsx
@@ -1,11 +1,9 @@
 
 function Clasificacion({resultado}) {
-function manexadorBoton() {
-    window.location.reload
-}
+
     return(
         <>
-        <button onClick={manexadorBoton}>Vuelve a votar</button>
+       
         <li>
             {resultado.pais} {resultado.puntos}
         </li>
