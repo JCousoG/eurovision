e74059098d8a37db31241019c6d236d836ced47d
Couso <jcousog@hotmail.com>
2023-06-23 16:21:05 +0200

    cambio a alineacion esquerda das cancions

--- frontend/src/components/cancion.jsx
+++ frontend/src/components/cancion.jsx
@@ -7,7 +7,7 @@ function Cancion({cancions}) {
         <>
          <label>
 
-<li><h2>{cancions.pais}</h2><div style={{backgroundRepeat: "no-repeat", backgroundPosition: 'center', backgroundImage: `url(${cargando})`}}><Player ruta={cancions.url}></Player></div><Puntuacion id={cancions.id}></Puntuacion></li>
+<li><h2>{cancions.pais}</h2><div style={{backgroundRepeat: "no-repeat", backgroundPosition: 'left', backgroundImage: `url(${cargando})`}}><Player ruta={cancions.url}></Player></div><Puntuacion id={cancions.id}></Puntuacion></li>
 </label>
         </>
     )
